608c22c1c77fea6baf237fd50a12dcfd2a29256e
Lewis <hamiltonjlewis@gmail.com>
2021-08-29 15:55:19 -0700

    added working api call

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import axios from "axios";
+
 export default function App() {
+  const options = {
+    method: "GET",
+    url: "https://dad-jokes.p.rapidapi.com/random/joke/png",
+    headers: {
+      "x-rapidapi-host": "dad-jokes.p.rapidapi.com",
+      "x-rapidapi-key": process.env.REACT_APP_API_KEY,
+    },
+  };
+
   const call = () => {
-    console.log("hello");
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response.data.body.setup);
+        console.log(response.data.body.punchline);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
   };
   return (
     <div>
